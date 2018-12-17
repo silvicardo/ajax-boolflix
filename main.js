2984aad1324a1f26cee1642d20c7a672085022fc
silvicardo <silvicardo@gmail.com>
2018-12-17 12:32:37 +0100

    project template html css js

---
+++ main.js
@@ -0,0 +1,27 @@
+/**********************************************/
+/**********************************************/
+/****** PROGETTO: ajax-boolflix - main.js *****/
+/**********************************************/
+/**********************************************/
+
+/*
+themoviedb- API
+API KEY : e84c7d2ede59ead3397581c0ad7d4dec
+TEST URL : https://api.themoviedb.org/3/search/movie?api_key=<<api_key>>&language=en-US&page=1&include_adult=false
+Required params: api-key, query(String)
+*/
+
+/*****************************/
+/******** PROGRAMMA **********/
+/*****************************/
+
+$(document).ready(function () {
+  console.log('welcome to ajax-BoolFlix');
+
+
+
+});
+
+/**********************************/
+/*************FUNZIONI*************/
+/**********************************/
